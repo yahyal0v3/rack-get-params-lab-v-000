cc2c51381e0c36ef8833633d281b7b9d85842ec0
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-10 02:44:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -41,7 +41,7 @@ class Application
   def handle_adding(added_item)
     if @@items.include?(added_item)
       @@cart << added_item
-      return "added #{added_term}"
+      return "added #{added_item}"
     else
       return "We don't have that item"
     end
